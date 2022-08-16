6064e5430661004f10e5d223ebaa66bd8d7fe8cf
Colin Mollenhour <colin@mollenhour.com>
2022-08-16 15:55:51 -0400

    Change no-galera-healthcheck.sh to use HTTP 1.0 and Connection: close and remove end-close option. Refs #77

--- no-galera-healthcheck.sh
+++ no-galera-healthcheck.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 socat \
-    TCP-LISTEN:$1,crlf,reuseaddr,end-close,fork \
+    TCP-LISTEN:$1,crlf,reuseaddr,fork \
     SYSTEM:"
-        echo HTTP/1.1 200 OK;
-        echo Content-Type\: text/plain;
+        echo 'HTTP/1.0 200 OK';
+        echo 'Content-Type: text/plain';
+        echo 'Connection: close';
         echo;
-        echo \"Fake report: healthy\";
+        echo 'Fake report: healthy';
     "
